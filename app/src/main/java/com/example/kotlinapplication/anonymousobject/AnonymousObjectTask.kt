e3539522f1df6644fb64cd55dc663765a8b8c1a8
Mund3k <kubat1611@gmail.com>
2024-09-23 13:59:22 +0200

    AnonymousObject

--- app/src/main/java/com/example/kotlinapplication/anonymousobject/AnonymousObjectTask.kt
+++ app/src/main/java/com/example/kotlinapplication/anonymousobject/AnonymousObjectTask.kt
@@ -3,11 +3,13 @@ package com.example.kotlinapplication.anonymousobject
 data class Person(val name: String, val age: Int, val height: Double)
 
 fun sortPeople(people: List<Person>, comparator: Comparator<Person>): List<Person> {
+
     return people.sortedWith(comparator)
 }
 
 // TODO:
 // 1. Dodaj komparatory dla imion, wieku i wzrostu
+
 
 fun main() {
     val people = listOf(
@@ -17,13 +19,28 @@ fun main() {
         Person("Diana", 22, 5.7)
     )
 
+
+
+
     // TODO: odkomentuj
-//    println("Sorted by name:")
-//    sortPeople(people, nameComparator).forEach { println(it) }
-//
-//    println("\nSorted by age:")
-//    sortPeople(people, ageComparator).forEach { println(it) }
-//
-//    println("\nSorted by height:")
-//    sortPeople(people, heightComparator).forEach { println(it) }
+    println("Sorted by name:")
+    sortPeople(people, object: Comparator<Person> {
+        override fun compare(p0: Person?, p1: Person?): Int {
+           return p0!!.name.compareTo(p1!!.name)
+        }
+    }).forEach { println(it) }
+
+    println("\nSorted by age:")
+    sortPeople(people, object: Comparator<Person>{
+        override fun compare(p0: Person?, p1: Person?): Int {
+            return p0!!.age.compareTo(p1!!.age)
+        }
+    }).forEach { println(it) }
+
+    println("\nSorted by height:")
+    sortPeople(people, object: Comparator<Person>{
+        override fun compare(p0: Person?, p1: Person?): Int {
+            return p0!!.height.compareTo(p1!!.height)
+        }
+    }).forEach { println(it) }
 }
