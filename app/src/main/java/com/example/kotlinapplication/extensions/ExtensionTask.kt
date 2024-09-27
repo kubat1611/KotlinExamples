e0712683d9b3c8c37510af0d30730feaf5f92e9a
Mund3k <kubat1611@gmail.com>
2024-09-27 13:15:08 +0200

    ExtensionTask

--- app/src/main/java/com/example/kotlinapplication/extensions/ExtensionTask.kt
+++ app/src/main/java/com/example/kotlinapplication/extensions/ExtensionTask.kt
@@ -3,6 +3,12 @@ package com.example.kotlinapplication.extensions
 //TODO:
 // 1. Napisz extension function dla listy intów, która policzy ich średnią
 
+
+fun List<Int>.average() : Double{
+    return this.sum().toDouble() / this.size
+}
+
+
 fun main() {
     val numbers = listOf(10, 20, 30, 40, 50)
     val averageValue = numbers.average()
