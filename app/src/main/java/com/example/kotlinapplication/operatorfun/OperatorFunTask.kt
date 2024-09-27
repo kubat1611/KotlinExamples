1cd379164f7ddde2b2669c39a36626c24bd3d034
Mund3k <kubat1611@gmail.com>
2024-09-27 10:39:20 +0200

    OperatorFunTask

--- app/src/main/java/com/example/kotlinapplication/operatorfun/OperatorFunTask.kt
+++ app/src/main/java/com/example/kotlinapplication/operatorfun/OperatorFunTask.kt
@@ -2,10 +2,15 @@ package com.example.kotlinapplication.operatorfun
 
 // TODO:
 // 1. Napisz klasę Multiplier, która będzie wykorzystywać operator fun
+class Multiplier {
+    operator fun invoke(x : Int, y : Int) : Int {
+        return x * y
+    }
+}
 
 fun main() {
 // TODO: Odkomentuj
-//    val multiplier = Multiplier()
-//    val result = multiplier(4, 5)
-//    println("The product of 4 and 5 is: $result")
+    val multiplier = Multiplier()
+    val result = multiplier(4, 5)
+    println("The product of 4 and 5 is: $result")
 }
