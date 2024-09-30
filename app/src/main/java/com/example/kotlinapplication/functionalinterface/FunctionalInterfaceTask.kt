1174cf504c1a3177583b2e05576afaaba04853a6
Mund3k <kubat1611@gmail.com>
2024-09-30 11:10:13 +0200

    FunctionalInterfaceTask

--- app/src/main/java/com/example/kotlinapplication/functionalinterface/FunctionalInterfaceTask.kt
+++ app/src/main/java/com/example/kotlinapplication/functionalinterface/FunctionalInterfaceTask.kt
@@ -4,6 +4,15 @@ package com.example.kotlinapplication.functionalinterface
 // Zdefiniuj functional interface dla operacji arytmetycznych
 // Funkcja powinna przyjmować dwie liczby jako parametry i zwracać wynik
 
+fun interface Operation {
+    fun params(x: Int, y : Int) : Int
+}
+
+fun performOperation(a: Int, b: Int, operation: Operation): Int{
+    return operation.params(a,b)
+}
+
+
 
 // TODO:
 // Zaimplementuj funkcję pomocniczą, która wywołuje funkcję z interfasu
@@ -11,15 +20,16 @@ package com.example.kotlinapplication.functionalinterface
 
 fun main() {
     // TODO: Odkomentuj
-//    val addition: Operation =
-//    val multiplication: Operation =
-//    val subtraction: Operation =
-//
-//    println("Addition: ${performOperation(5, 3, addition)}")
-//    println("Multiplication: ${performOperation(5, 3, multiplication)}")
-//    println("Subtraction: ${performOperation(5, 3, subtraction)}")
-//
-//    println("Division: ${performOperation(10, 2, )}")
+    val addition: Operation = Operation { x,y -> x + y }
+    val multiplication: Operation = Operation {x,y -> x * y}
+    val subtraction: Operation = Operation {x,y -> x - y}
+    val division: Operation = Operation{x,y -> x/y}
+
+    println("Addition: ${performOperation(5, 3, addition)}")
+    println("Multiplication: ${performOperation(5, 3, multiplication)}")
+    println("Subtraction: ${performOperation(5, 3, subtraction)}")
+
+    println("Division: ${performOperation(10, 2, division)}")
 }
 
 // TODO*
