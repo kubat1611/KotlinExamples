7008020cd79f2cc0a05270e196a0536ccaeec75f
Mund3k <kubat1611@gmail.com>
2024-09-30 13:26:15 +0200

    EnumTask2

--- app/src/main/java/com/example/kotlinapplication/enums/EnumTask2.kt
+++ app/src/main/java/com/example/kotlinapplication/enums/EnumTask2.kt
@@ -5,17 +5,27 @@ package com.example.kotlinapplication.enums
 // Są możliwe 4 statusy: Pending, Shipped, Delivered i Cancelled
 // Delivered i Cancelled są statusami końcowymi, a Pending i Shipped nie
 
+enum class OrderStatus(val status: Boolean) {
+    PENDING(false),
+    SHIPPED(false),
+    DELIVERED(true),
+    CANCELLED(true)
+
+}
+
 // TODO:
 // Znajdż instancję enum klasy za pomocą jej nazwy
 // Zbuduj stringa opisującego jej parametr, nazwę i kolejność
 // Jeśli wariant enum klasy o podanej nazwie nie istnieje, zwróć stringa opisującego błąd
 fun getStatusByName(statusName: String): String {
-    return ""
+    for (status in OrderStatus.entries)
+        if (status.name == statusName.uppercase()) return "Your order status is  ${status.name}, your order is finished: ${status.status}, PENDING -> SHIPPED -> DELIVERED or CANCELLED"
+    return "No status found"
 }
 
 // TODO: wypisz wszystkie warianty enum klasy
 fun printOrderStatuses() {
-
+    for (status in OrderStatus.entries) println("${status}, ${status.status}")
 }
 
 fun main() {
